fcd2e59dce9daf45eeff6b014cfc28a98713d971
Nithish Kumar T S <knithish938@gmail.com>
2025-08-03 22:17:28 +0530

    Create vowel_sorter.py

---
+++ vowel_sorter.py
@@ -0,0 +1,16 @@
+# Program to extract vowels from an input string and print them in the order 'aeiou', 
+# with each vowel repeated according to its frequency in the string.
+# Example: Input = "communication" → Output = "aiioou"
+s=input("Enter a string: ")
+v="aeiou"
+d={}
+s1=""
+for i in s:
+    if i not in d and i in v:
+        d[i]=1
+    elif i in d and i in v:
+        d[i]=d[i]+1
+for i in v:
+    if i in d:
+        s1=s1+(d[i]*i)
+print(s1)
